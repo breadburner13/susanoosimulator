32e3ce2abe1a018ec48e52322bdbd365397b3016
breadburner13 <70679117+breadburner13@users.noreply.github.com>
2020-11-01 06:03:37 -0800

    Add files via upload
    clicking on screen now randomizes color of rain

---
+++ drop.js
@@ -0,0 +1,38 @@
+// Daniel Shiffman
+// http://codingtra.in
+// http://patreon.com/codingtrain
+// Code for: https://youtu.be/KkyIDI6rQJI
+
+function Drop() {
+  this.x = random(width);
+  this.y = random(-500, -50);
+  this.z = random(0, 20);
+  this.len = map(this.z, 0, 20, 10, 20);
+  this.r = 138;
+  this.g = 43;
+  this.b = 226;
+  this.yspeed = map(this.z, 0, 20, 1, 20);
+
+  this.fall = function() {
+    this.y = this.y + this.yspeed;
+    var grav = map(this.z, 0, 20, 0, 0.2);
+    this.yspeed = this.yspeed + grav;
+
+    if (this.y > height) {
+      this.y = random(-200, -100);
+      this.yspeed = map(this.z, 0, 20, 4, 10);
+    }
+  };
+
+  this.show = function() {
+    var thick = map(this.z, 0, 20, 1, 3);
+    strokeWeight(thick);
+    stroke(this.r, this.g, this.b);
+    line(this.x, this.y, this.x, this.y + this.len);
+  };
+  this.changeColor = function() {
+    this.r = random(1, 255);
+    this.g = random(1, 255);
+    this.b = random(1, 255);
+  }
+}
