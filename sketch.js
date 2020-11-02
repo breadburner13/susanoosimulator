249203f888d55f20d40ad53c6d880375a2636a61
BreadBurner13 <jungkrischan@gmail.com>
2020-11-01 23:39:21 -0800

    added changing colors based on text input

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 var drops = [];
-var command;
+var commandprompt;
+var splitcommands;
 var startrainbutton;
 var stoprainbutton;
 var changecolorbutton;
@@ -10,18 +11,19 @@ function setup() {
   for (var i = 0; i < 500; i++) {
     drops[i] = new Drop();
   }
-  createElement("h1", "let there be...");
-  command = createInput("...");
-  startrainbutton = createButton("start rain");
-  stoprainbutton = createButton("stop rain");
-  changecolorbutton = createButton("change color");
+  createElement("h1", "change color to...");
+  commandprompt = createInput("");  
+  commandprompt.changed(processCommands);
+  // startrainbutton = createButton("start rain");
+  // stoprainbutton = createButton("stop rain");
+  // changecolorbutton = createButton("change color");
 }
 
 function draw() {
   background(230, 230, 250);
-  startrainbutton.mousePressed(neverEndingRain);
-  stoprainbutton.mousePressed(theRainStopped);
-  changecolorbutton.mousePressed(showtime);
+  // startrainbutton.mousePressed(neverEndingRain);
+  // stoprainbutton.mousePressed(theRainStopped);
+  // changecolorbutton.mousePressed(showtime);
   for (var i = 0; i < drops.length; i++) {
     drops[i].fall();
     drops[i].show();
@@ -42,5 +44,72 @@ function showtime () {
   newcolor = random(colorList);
   for (var i = 0; i < drops.length; i++) {
     drops[i].changeColor(newcolor);
+  }
+}
+
+function processCommands() {
+  splitcommands = split(this.value(), ' ');
+  for (acommand of splitcommands) {
+    print(acommand);
+    if (acommand == "red") {
+      this.newcolor = colorList[0];
+      for (var i = 0; i < drops.length; i++) {
+        drops[i].changeColor(this.newcolor);
+      }
+    }
+    if (acommand == "orange") {
+      this.newcolor = colorList[1];
+      for (var i = 0; i < drops.length; i++) {
+        drops[i].changeColor(this.newcolor);
+      }
+    }
+    if (acommand == "yellow") {
+      this.newcolor = colorList[2];
+      for (var i = 0; i < drops.length; i++) {
+        drops[i].changeColor(this.newcolor);
+      }
+    }
+    if (acommand == "green") {
+      this.newcolor = colorList[3];
+      for (var i = 0; i < drops.length; i++) {
+        drops[i].changeColor(this.newcolor);
+      }
+    }
+    if (acommand == "blue") {
+      this.newcolor = colorList[4];
+      for (var i = 0; i < drops.length; i++) {
+        drops[i].changeColor(this.newcolor);
+      }
+    }
+    if (acommand == "indigo") {
+      this.newcolor = colorList[5];
+      for (var i = 0; i < drops.length; i++) {
+        drops[i].changeColor(this.newcolor);
+      }
+    }
+    if (acommand == "violet") {
+      this.newcolor = colorList[6];
+      for (var i = 0; i < drops.length; i++) {
+        drops[i].changeColor(this.newcolor);
+      }
+    }
+    if (acommand == "purple") {
+      this.newcolor = colorList[7];
+      for (var i = 0; i < drops.length; i++) {
+        drops[i].changeColor(this.newcolor);
+      }
+    }
+    if (acommand == "white") {
+      this.newcolor = colorList[8];
+      for (var i = 0; i < drops.length; i++) {
+        drops[i].changeColor(this.newcolor);
+      }
+    }
+    if (acommand == "black") {
+      this.newcolor = colorList[9];
+      for (var i = 0; i < drops.length; i++) {
+        drops[i].changeColor(this.newcolor);
+      }
+    }
   }
 }
