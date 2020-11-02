41490d348d384f8c865735de26c5c3cf788c52b2
BreadBurner13 <jungkrischan@gmail.com>
2020-11-01 21:48:07 -0800

    created command for red rain

--- sketch.js
+++ sketch.js
@@ -1,11 +1,14 @@
 var drops = [];
 var r, g, b; 
+var command;
 
 function setup() {
   createCanvas(640, 360);
   for (var i = 0; i < 500; i++) {
     drops[i] = new Drop();
   }
+  createElement("h1", "let there be...");
+  command = createInput("...");
 }
 
 function draw() {
@@ -14,9 +17,17 @@ function draw() {
     drops[i].fall();
     drops[i].show();
   }
-  if(keyIsPressed) {
+  // if(keyIsPressed) {
+  //   for (drop of drops) {
+  //     drop.randomChange();
+  //   }
+  // }
+  if (command.value() == "red") {
+    this.r = 255;
+    this.g = 0;
+    this.b = 0;
     for (drop of drops) {
-      drop.randomChange();
+      drop.changeColor(this.r, this.g, this.b);
     }
   }
 }
